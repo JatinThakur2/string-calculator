fa599da36b3d90321d359b10ceb7b063eafe4cac
Jatin Thakur <jatinthakur3333@gmail.com>
2025-03-27 10:49:00 +0530

    Test for two numbers input

--- src/calculator.js
+++ src/calculator.js
@@ -3,5 +3,10 @@ export function add(numbers) {
     return 0;
   }
 
+  if (numbers.includes(",")) {
+    const nums = numbers.split(",").map((num) => parseInt(num, 10));
+    return nums[0] + nums[1];
+  }
+
   return parseInt(numbers, 10);
 }
