ccf9ab428e546364ae1be65c313a67001db7b948
Jatin Thakur <jatinthakur3333@gmail.com>
2025-03-27 10:47:42 +0530

    Test for single number input

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,5 +1,11 @@
+import { add } from "./calculator";
+
 describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
+  });
+
+  test("should return the number for a single number", () => {
+    expect(add("1")).toBe(1);
   });
 });
